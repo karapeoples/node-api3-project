0a8d5e65a3df93976df14a721c2804e3f87b1f7f
karapeoples <52808542+karapeoples@users.noreply.github.com>
2020-03-20 22:59:19 -0500

    Let's see if this works

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
-// code away!
+// code away!const favicon = require('serve-favicon')
+require('dotenv').config()
 const server = require('./server.js')
 
-const PORT = 4994
-server.listen(PORT, () => {
-	console.log(`\n*^^*~~Server Running on http://localhost:${PORT}~~*^^*\n`)
+const port = process.env.PORT || 4994
+
+server.listen(port, () => {
+	console.log(`\n*^^*~~Server Running on http://localhost:${port}~~*^^*\n`)
 })
